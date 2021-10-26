fddb427d4f55b63a700bae15069bfbcc7aeb7c6f
Vladislav <vladislav.koselev@ivkhk.ee>
2021-10-26 11:14:08 +0300

    nachalo nachal

---
+++ model/booksArray.php
@@ -0,0 +1,16 @@
+<?php
+$books = array(
+            array("bookname" => "HTML and CSS",
+                    "author" => "Jon Duckett",
+                    "price" => "17.195",
+                    "description" => "Every day, more and more people want to learn some HTML and CSS. Joining the profess"
+                    "image" => "HTMLandCSS_Duckett.jpg"),
+            array("bookname" => "HTML5",
+                    "author" => "Matthew MacDonald",
+                    "price" => "27.65$",
+                    "description" => "HTML5 is more than a markup language-it's a collection of several idependent")
+                    "image"
+            
+            
+            )
+)
