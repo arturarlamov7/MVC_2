ecebad374eb9b83e41d47a1e90dd28666859cee6
Vladislav <vladislav.koselev@ivkhk.ee>
2021-11-01 09:12:40 +0200

    sss

--- model/booksArray.php
+++ model/booksArray.php
@@ -8,16 +8,21 @@ $books = array(
             array("bookname" => "HTML5",
                     "author" => "Matthew MacDonald",
                     "price" => "27.65$",
-                    "description" => "HTML5 is more than a markup language-it's a collection of several idependent")
+                    "description" => "HTML5 is more than a markup language-it's a collection of several idependent",
                     "image" => "HTML5TheMissing_MacDonald.jpg"),
             array("bookname" => "Head First HTML5 Programming",
                     "author" => "Eric Freeman, Elisabeth Robson",
                     "price" => "35.35$",
-                    "description" => "HTML has been on a wild ride. Sure, HTML started as a mere markup language, but more")
-                    "image" => "HTML5TheMissing_MacDonald.jpg"),                    
-
-            )
-            
-            
-            )
-)
+                    "description" => "HTML has been on a wild ride. Sure, HTML started as a mere markup language, but more",
+                    "image" => "HeadFirstHTML5.jpg"),  
+            array("bookname" => "Head First JavaScript Programming",
+                    "author" => "Eric Freeman, Elisabeth Robson",
+                    "price" => "37.60$",
+                    "description" => "This brain-friendly guide teaches you everything from JS language fundamental",
+                    "image" => "HeadFirstJavaScript.jpg"),
+            array("bookname" => "jQuery in Action",
+                    "author" => "Bear Bibeault, Yehuda Katz",
+                    "price" => "32.12$",
+                    "description" => "A really good web development framework anticipates your needs. jQuery does more-it",
+                    "image" => "JQueryInAction.jpg"));
+?>
--- view/templates/layout.php
+++ view/templates/layout.php
@@ -4,20 +4,30 @@
 <link href="public/css/mystyle.css" rel="stylesheet">
 </head>
 <body>
-    <div class="container">
-        <div class="header clearfix">
-    <h2>Книжный магазин</h2>
-        <h4 class="text-muted">
+        <div class="container">
+            <div class="header clearfix">
+        <h2>Книжный магазин</h2>
+                <h4 class="text-muted">
+                    <?php
+                        echo '<a href="./>Главная</a> &#187';
+                        echo '<a href="books>Книга</a>';
+                        if (!empty($book['bookname'])) echo ' &#187'.$book['bookname'];
+                    ?>
+
+
+                </h4>
+            </div>
+            <div id="content" style="padding-top:20px;">
             <?php
-                echo '<a href="./>Главная</a> &#187';
-                echo '<a href="books>Книга</a>';
-                if (!empty($book['bookname'])) echo ' &#187'.$book['bookname'];
+                    echo $content; 
+            
             ?>
 
 
-        </h4>
+            </div>
+            <footer class="footer">
+                <p>&copy; Year Design <i class="fa fa-child"></i></p>
+            </footer>
         </div>
-        
-    </div>
 </body>
 </html>
